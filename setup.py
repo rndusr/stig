5b62109efe13612c3f67d8f471fc00adb870cfeb
Random User <rndusr@posteo.de>
2017-01-18 13:43:12 +0100

    Bump minimum urwidtrees version to 1.0.3.dev0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires = [
         'urwid>=1.3.0',
         'aiohttp>=0.22.5',
-        'urwidtrees>=1.0.1.1',
+        'urwidtrees>=1.0.3dev0',
         'appdirs',
         'blinker',
     ],
