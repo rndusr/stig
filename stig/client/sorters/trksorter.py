de0c151da519fb310f56adfe73b51aef99b78b79
Random User <rndusr@posteo.de>
2017-08-03 16:30:02 +0200

    Add TorrentTrackerSorter

---
+++ stig/client/sorters/trksorter.py
@@ -0,0 +1,34 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+# http://www.gnu.org/licenses/gpl-3.0.txt
+
+from . import (SortSpecBase, SorterBase)
+
+class _SortSpec(SortSpecBase):
+    def __init__(self, *args, description='', **kwargs):
+        description = 'Sort tracker by %s' % description
+        super().__init__(*args, description=description, **kwargs)
+
+
+class TorrentTrackerSorter(SorterBase):
+    SORTSPECS = {
+        'tier':       _SortSpec(lambda t: t['tier'],
+                                description='tier number'),
+        'domain':     _SortSpec(lambda t: t['domain'],
+                                description='domain from announce URL',
+                                aliases=('host',)),
+        'error':      _SortSpec(lambda t: t['error'],
+                                description='error message'),
+        'state':      _SortSpec(lambda t: t['state'],
+                                description='tracker state'),
+        'torrent':    _SortSpec(lambda t: t['tname'].lower(),
+                                description='torrent name'),
+    }
+    DEFAULT_SORT = 'torrent'
