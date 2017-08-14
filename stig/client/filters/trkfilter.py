44528fb86d26bb40b602203fe7b7d58571edaa90
Random User <rndusr@posteo.de>
2017-08-14 17:14:50 +0200

    Reference TorrentTracker.TYPES in global variable

--- stig/client/filters/trkfilter.py
+++ stig/client/filters/trkfilter.py
@@ -12,6 +12,7 @@
 """Filtering TrackerList items by various values"""
 
 from ..ttypes import TorrentTracker
+VALUETYPES = TorrentTracker.TYPES
 from . import (BoolFilterSpec, CmpFilterSpec, Filter, FilterChain)
 
 
@@ -33,40 +34,40 @@ class SingleTrackerFilter(Filter):
         'tier': CmpFilterSpec(
             lambda trk, op, v: op(trk['tier'], v),
             description='Match VALUE against torrent tier',
-            value_type=TorrentTracker.TYPES['tier']),
+            value_type=VALUETYPES['tier']),
         'domain': CmpFilterSpec(
             lambda trk, op, v: op(trk['domain'], v),
             description='Match VALUE against domain from announce URL',
             aliases=('host',),
-            value_type=TorrentTracker.TYPES['domain']),
+            value_type=VALUETYPES['domain']),
         'url-announce': CmpFilterSpec(
             lambda trk, op, v: op(trk['url-announce'], v),
             description='Match VALUE against announce URL',
-            value_type=TorrentTracker.TYPES['url-announce']),
+            value_type=VALUETYPES['url-announce']),
         'url-scrape': CmpFilterSpec(
             lambda trk, op, v: op(trk['url-scrape'], v),
             description='Match VALUE against scrape URL',
-            value_type=TorrentTracker.TYPES['url-scrape']),
+            value_type=VALUETYPES['url-scrape']),
         'state': CmpFilterSpec(
             lambda trk, op, v: op(trk['state'], v),
             description="Match VALUE against tracker state (inactive, waiting, queued, announcing, scraping)",
-            value_type=TorrentTracker.TYPES['error']),
+            value_type=VALUETYPES['error']),
         'error': CmpFilterSpec(
             lambda trk, op, v: op(trk['error'], v),
             description='Match VALUE against error message from tracker',
-            value_type=TorrentTracker.TYPES['error']),
+            value_type=VALUETYPES['error']),
         'downloads': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-downloads'], v),
             description='Match VALUE against number of known downloads',
-            value_type=TorrentTracker.TYPES['count-downloads']),
+            value_type=VALUETYPES['count-downloads']),
         'leeches': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-leeches'], v),
             description='Match VALUE against number of known downloading peers',
-            value_type=TorrentTracker.TYPES['count-leeches']),
+            value_type=VALUETYPES['count-leeches']),
         'seeds': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-seeds'], v),
             description='Match VALUE against number of known seeding peers',
-            value_type=TorrentTracker.TYPES['count-seeds']),
+            value_type=VALUETYPES['count-seeds']),
     }
 
 
