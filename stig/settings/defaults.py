2d2ab7afeeee1e3cb5f4982f3362b1d29b8c808b
Random User <rndusr@posteo.de>
2017-08-15 15:14:18 +0200

    Fix spacing

--- stig/settings/defaults.py
+++ stig/settings/defaults.py
@@ -219,9 +219,9 @@ DEFAULT_KEYMAP = (
     {'context': 'main', 'key': 'shift-left',  'action': 'set srv.limit.rate.up -=100kB'},
 
     # Tab management
-    {'context': 'tabs', 'key': 'n', 'action': 'tab'},
-    {'context': 'tabs', 'key': 'd', 'action': 'tab --close'},
-    {'context': 'tabs', 'key': 'D', 'action': 'tab --close --focus left'},
+    {'context': 'tabs', 'key': 'n',      'action': 'tab'},
+    {'context': 'tabs', 'key': 'd',      'action': 'tab --close'},
+    {'context': 'tabs', 'key': 'D',      'action': 'tab --close --focus left'},
     {'context': 'tabs', 'key': 'meta-1', 'action': 'tab --focus 1'},
     {'context': 'tabs', 'key': 'meta-2', 'action': 'tab --focus 2'},
     {'context': 'tabs', 'key': 'meta-3', 'action': 'tab --focus 3'},
