408747aa369a3bd06bd6297c4a6d41f52cb3d9e3
Random User <rndusr@posteo.de>
2017-09-06 14:27:11 +0200

    Add 'needed_keys' to tracker filers
    We need those to be able to use comparative filters as boolean filters if no
    value is given (e.g. `lstrk all leeches` lists trackers from all torrents with
    more than 0 leeches (i.e. True)).

--- stig/client/filters/trkfilter.py
+++ stig/client/filters/trkfilter.py
@@ -34,59 +34,72 @@ class SingleTrackerFilter(Filter):
         'tier': CmpFilterSpec(
             lambda trk, op, v: op(trk['tier'], v),
             description='Match VALUE against torrent tier',
+            needed_keys=('tier',),
             value_type=VALUETYPES['tier']),
         'domain': CmpFilterSpec(
             lambda trk, op, v: op(trk['domain'], v),
             description='Match VALUE against domain from announce URL',
             aliases=('host',),
+            needed_keys=('domain',),
             value_type=VALUETYPES['domain']),
         'url-announce': CmpFilterSpec(
             lambda trk, op, v: op(trk['url-announce'], v),
             description='Match VALUE against announce URL',
+            needed_keys=('url-announce',),
             value_type=VALUETYPES['url-announce']),
         'url-scrape': CmpFilterSpec(
             lambda trk, op, v: op(trk['url-scrape'], v),
             description='Match VALUE against scrape URL',
+            needed_keys=('url-scrape',),
             value_type=VALUETYPES['url-scrape']),
         'state': CmpFilterSpec(
             lambda trk, op, v: op(trk['state'], v),
             description="Match VALUE against tracker state (inactive, waiting, queued, announcing, scraping)",
+            needed_keys=('state',),
             value_type=VALUETYPES['error']),
         'error': CmpFilterSpec(
             lambda trk, op, v: op(trk['error'], v),
             description='Match VALUE against error message from tracker',
+            needed_keys=('error',),
             value_type=VALUETYPES['error']),
         'downloads': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-downloads'], v),
             description='Match VALUE against number of known downloads',
+            needed_keys=('count-downloads',),
             value_type=VALUETYPES['count-downloads']),
         'leeches': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-leeches'], v),
             description='Match VALUE against number of known downloading peers',
+            needed_keys=('count-leeches',),
             value_type=VALUETYPES['count-leeches']),
         'seeds': CmpFilterSpec(
             lambda trk, op, v: op(trk['count-seeds'], v),
             description='Match VALUE against number of known seeding peers',
+            needed_keys=('count-seeds',),
             value_type=VALUETYPES['count-seeds']),
         'last-announce': CmpFilterSpec(
             lambda trk, op, v: op(trk['time-last-announce'], v),
             description='Match VALUE against last time of a successful announce',
             value_type=VALUETYPES['time-last-announce'],
+            needed_keys=('time-last-announce',),
             value_convert=VALUETYPES['time-last-announce'].from_string),
         'next-announce': CmpFilterSpec(
             lambda trk, op, v: op(trk['time-next-announce'], v),
             description='Match VALUE against next time of a successful announce',
             value_type=VALUETYPES['time-next-announce'],
+            needed_keys=('time-next-announce',),
             value_convert=VALUETYPES['time-next-announce'].from_string),
         'last-scrape': CmpFilterSpec(
             lambda trk, op, v: op(trk['time-last-scrape'], v),
             description='Match VALUE against last time of a successful scrape',
             value_type=VALUETYPES['time-last-scrape'],
+            needed_keys=('time-last-scrape',),
             value_convert=VALUETYPES['time-last-scrape'].from_string),
         'next-scrape': CmpFilterSpec(
             lambda trk, op, v: op(trk['time-next-scrape'], v),
             description='Match VALUE against next time of a successful scrape',
             value_type=VALUETYPES['time-next-scrape'],
+            needed_keys=('time-next-scrape',),
             value_convert=VALUETYPES['time-next-scrape'].from_string),
     }
 
