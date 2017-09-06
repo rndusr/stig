c4c5b535559769d1e01e3f889a589826eb7c02ec
Random User <rndusr@posteo.de>
2017-09-06 15:07:46 +0200

    Use make_cmp_filter() helper for tracker filter

--- stig/client/filters/trkfilter.py
+++ stig/client/filters/trkfilter.py
@@ -13,8 +13,11 @@
 
 from ..ttypes import TorrentTracker
 VALUETYPES = TorrentTracker.TYPES
-from . import (BoolFilterSpec, CmpFilterSpec, Filter, FilterChain)
+from . import (BoolFilterSpec, CmpFilterSpec, Filter, FilterChain, make_cmp_filter)
 
+
+def _make_cmp_filter(*args, **kwargs):
+    return make_cmp_filter(TorrentTracker.TYPES, *args, **kwargs)
 
 class SingleTrackerFilter(Filter):
     DEFAULT_FILTER = 'domain'
@@ -31,76 +34,33 @@ class SingleTrackerFilter(Filter):
     }
 
     COMPARATIVE_FILTERS = {
-        'tier': CmpFilterSpec(
-            lambda trk, op, v: op(trk['tier'], v),
-            description='Match VALUE against torrent tier',
-            needed_keys=('tier',),
-            value_type=VALUETYPES['tier']),
-        'domain': CmpFilterSpec(
-            lambda trk, op, v: op(trk['domain'], v),
-            description='Match VALUE against domain from announce URL',
-            aliases=('host',),
-            needed_keys=('domain',),
-            value_type=VALUETYPES['domain']),
-        'url-announce': CmpFilterSpec(
-            lambda trk, op, v: op(trk['url-announce'], v),
-            description='Match VALUE against announce URL',
-            needed_keys=('url-announce',),
-            value_type=VALUETYPES['url-announce']),
-        'url-scrape': CmpFilterSpec(
-            lambda trk, op, v: op(trk['url-scrape'], v),
-            description='Match VALUE against scrape URL',
-            needed_keys=('url-scrape',),
-            value_type=VALUETYPES['url-scrape']),
-        'state': CmpFilterSpec(
-            lambda trk, op, v: op(trk['state'], v),
-            description="Match VALUE against tracker state (inactive, waiting, queued, announcing, scraping)",
-            needed_keys=('state',),
-            value_type=VALUETYPES['error']),
-        'error': CmpFilterSpec(
-            lambda trk, op, v: op(trk['error'], v),
-            description='Match VALUE against error message from tracker',
-            needed_keys=('error',),
-            value_type=VALUETYPES['error']),
-        'downloads': CmpFilterSpec(
-            lambda trk, op, v: op(trk['count-downloads'], v),
-            description='Match VALUE against number of known downloads',
-            needed_keys=('count-downloads',),
-            value_type=VALUETYPES['count-downloads']),
-        'leeches': CmpFilterSpec(
-            lambda trk, op, v: op(trk['count-leeches'], v),
-            description='Match VALUE against number of known downloading peers',
-            needed_keys=('count-leeches',),
-            value_type=VALUETYPES['count-leeches']),
-        'seeds': CmpFilterSpec(
-            lambda trk, op, v: op(trk['count-seeds'], v),
-            description='Match VALUE against number of known seeding peers',
-            needed_keys=('count-seeds',),
-            value_type=VALUETYPES['count-seeds']),
-        'last-announce': CmpFilterSpec(
-            lambda trk, op, v: op(trk['time-last-announce'], v),
-            description='Match VALUE against last time of a successful announce',
-            value_type=VALUETYPES['time-last-announce'],
-            needed_keys=('time-last-announce',),
-            value_convert=VALUETYPES['time-last-announce'].from_string),
-        'next-announce': CmpFilterSpec(
-            lambda trk, op, v: op(trk['time-next-announce'], v),
-            description='Match VALUE against next time of a successful announce',
-            value_type=VALUETYPES['time-next-announce'],
-            needed_keys=('time-next-announce',),
-            value_convert=VALUETYPES['time-next-announce'].from_string),
-        'last-scrape': CmpFilterSpec(
-            lambda trk, op, v: op(trk['time-last-scrape'], v),
-            description='Match VALUE against last time of a successful scrape',
-            value_type=VALUETYPES['time-last-scrape'],
-            needed_keys=('time-last-scrape',),
-            value_convert=VALUETYPES['time-last-scrape'].from_string),
-        'next-scrape': CmpFilterSpec(
-            lambda trk, op, v: op(trk['time-next-scrape'], v),
-            description='Match VALUE against next time of a successful scrape',
-            value_type=VALUETYPES['time-next-scrape'],
-            needed_keys=('time-next-scrape',),
-            value_convert=VALUETYPES['time-next-scrape'].from_string),
+        'tier'           : _make_cmp_filter('tier',
+                                            description='Match VALUE against torrent tier'),
+        'domain'         : _make_cmp_filter('domain', aliases=('host',),
+                                            description='Match VALUE against domain from announce URL'),
+        'url-announce'   : _make_cmp_filter('url-announce',
+                                            description='Match VALUE against announce URL'),
+        'url-scrape'     : _make_cmp_filter('url-scrape',
+                                            description='Match VALUE against scrape URL'),
+        'state'          : _make_cmp_filter('state',
+                                            description=('Match VALUE against tracker state '
+                                                         '(inactive, waiting, queued, announcing, scraping)')),
+        'error'          : _make_cmp_filter('error',
+                                            description='Match VALUE against error message from tracker'),
+        'downloads'      : _make_cmp_filter('count-downloads',
+                                            description='Match VALUE against number of known downloads'),
+        'leeches'        : _make_cmp_filter('count-leeches',
+                                            description='Match VALUE against number of known downloading peers'),
+        'seeds'          : _make_cmp_filter('count-seeds',
+                                            description='Match VALUE against number of known seeding peers'),
+        'last-announce'  : _make_cmp_filter('time-last-announce',
+                                            description='Match VALUE against last time of a successful announce'),
+        'next-announce'  : _make_cmp_filter('time-next-announce',
+                                            description='Match VALUE against time of next announce'),
+        'last-scrape'    : _make_cmp_filter('time-last-scrape',
+                                            description='Match VALUE against last time of a successful scrape'),
+        'next-scrape'    : _make_cmp_filter('time-next-scrape',
+                                            description='Match VALUE against time of next scrape'),
     }
 
 
