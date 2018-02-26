49f1bcb7390f9d80922d03f3bdd21491f9714f43
Random User <rndusr@posteo.de>
2018-02-26 14:13:33 +0100

    New minimum version of aiohttp is 3.x

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires = [
         'urwid>=1.3.0',
         'urwidtrees>=1.0.3dev0',
-        'aiohttp>=2.3.6',
+        'aiohttp>=3',
         'pyxdg',
         'blinker',
         'natsort',
