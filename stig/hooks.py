c4c2eb1e0c1a2101f315c52f74fc0755d37eae17
Random User <rndusr@posteo.de>
2018-04-30 14:20:58 +0200

    Combine two assignments

--- stig/hooks.py
+++ stig/hooks.py
@@ -84,8 +84,7 @@ localcfg.on_change(_set_size_prefix, name='unitprefix.size')
 def _set_geoip(settings, name, value):
     if value and not geoip.available:
         log.error('Missing geoip dependency: maxminddb')
-        localcfg['geoip'] = False
-        value = False
+        localcfg['geoip'] = value = False
     geoip.enabled = value
 localcfg.on_change(_set_geoip, name='geoip')
 
