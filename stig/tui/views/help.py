b50e60f6a1ab95af988924c91fd37a00587e849e
Random User <rndusr@posteo.de>
2018-08-02 16:21:20 +0200

    Rename HelpText.secondary_filter -> search_phrase
    Also give HelpText instances a keymap context ('helptext').

--- stig/tui/views/help.py
+++ stig/tui/views/help.py
@@ -14,60 +14,65 @@ log = make_logger(__name__)
 
 import urwid
 from ...utils.string import strwidth
+from ...tui.main import keymap
 
 
 class HelpText(urwid.WidgetWrap):
+    keymap_context = 'helptext'
+
     def __init__(self, lines):
         self._lines = lines
-        self._pile = urwid.Pile([
-            urwid.Text(line) for line in lines
-        ])
-        super().__init__(self._pile)
+        self._search_phrase = None
+
+        self._Pile_Keymapped = keymap.wrap(urwid.Pile, context=self.keymap_context)
+        pile = self._Pile_Keymapped([urwid.Text(line) for line in lines])
+        super().__init__(pile)
 
     @property
-    def secondary_filter(self):
-        return getattr(self, '_secondary_filter', None)
+    def search_phrase(self):
+        return self._search_phrase
 
-    @secondary_filter.setter
-    def secondary_filter(self, term):
-        prev_secondary_filter = getattr(self, '_secondary_filter', None)
-        if (term is None or not str(term)) and prev_secondary_filter:
+    @search_phrase.setter
+    def search_phrase(self, phrase):
+        prev_search_phrase = self._search_phrase
+        if (phrase is None or not str(phrase)) and prev_search_phrase:
             self._clear_matches()
-        elif term != prev_secondary_filter:
-            self._highlight_matches(term)
+        elif phrase != prev_search_phrase:
+            self._highlight_matches(phrase)
 
     def _clear_matches(self):
-        self._secondary_filter = None
-        self._w = urwid.Pile([urwid.Text(line) for line in self._lines])
+        self._search_phrase = None
+        self._w = self._Pile_Keymapped([urwid.Text(line) for line in self._lines])
 
-    def _highlight_matches(self, term):
-        self._secondary_filter = term = str(term)
+    def _highlight_matches(self, phrase):
+        self._search_phrase = phrase = str(phrase)
 
-        term_cf = term.casefold()
-        case_sensitive = term != term_cf
+        phrase_cf = phrase.casefold()
+        case_sensitive = phrase != phrase_cf
         def match_boundaries(line):
-            """Yield (start, stop) tuple for each occurence of `term` in `line`"""
+            """Yield (start, stop) tuple for each occurence of `phrase` in `line`"""
             start = stop = -1
-            if not term:
+            if not phrase:
                 yield (start, stop)
 
-            def positions_of(term, line):
-                start = line.find(term)
+            def positions_of(phrase, line):
+                start = line.find(phrase)
+                log.debug('%r in %r: %r', phrase, line, phrase in line)
                 while start > -1:
-                    stop = start + strwidth(term)
+                    stop = start + strwidth(phrase)
                     yield (start, stop)
 
                     # Find next start
-                    offset = line[stop:].find(term)
+                    offset = line[stop:].find(phrase)
                     if offset > -1:
                         start = stop + offset
                     else:
                         start = -1
 
             if case_sensitive:
-                yield from positions_of(term, line)
+                yield from positions_of(phrase, line)
             else:
-                yield from positions_of(term_cf, line.casefold())
+                yield from positions_of(phrase_cf, line.casefold())
 
         texts = []
         for line in self._lines:
@@ -87,4 +92,4 @@ class HelpText(urwid.WidgetWrap):
             texts.append(urwid.Text(line_parts))
 
         # This calls _invalidate()
-        self._w = urwid.Pile(texts)
+        self._w = self._Pile_Keymapped(texts)
