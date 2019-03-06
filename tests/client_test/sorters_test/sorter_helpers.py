cd333030dbb95ea03e775ea93ad3d137fdce8527
Random User <rndusr@posteo.de>
2019-03-06 14:17:57 +0100

    Remove debugging prints

--- tests/client_test/sorters_test/sorter_helpers.py
+++ tests/client_test/sorters_test/sorter_helpers.py
@@ -7,12 +7,7 @@ class TestSorterBase(unittest.TestCase):
 
     def assert_sorted_ids(self, sorter_name, items, exp_ids):
         random.shuffle(items)
-        print(self.sorter_cls)
         sorter_names = (sorter_name,) + tuple(self.sorter_cls.SORTSPECS[sorter_name].aliases)
-        print('sorter_names:', sorter_names)
-        print('items:', items)
         for sort_str in sorter_names:
-            print('sort_str:', sort_str)
             sorter = self.sorter_cls((sort_str,))
-            print('sorter:', repr(sorter))
             self.assertEqual(tuple(item['id'] for item in sorter.apply(items)), exp_ids)
