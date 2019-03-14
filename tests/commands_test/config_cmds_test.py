4f44884d69fd59944b2641bed6de9cdabd837945
Random User <rndusr@posteo.de>
2019-03-14 14:22:09 +0100

    Move and improve tests for ratelimit command

--- tests/commands_test/config_cmds_test.py
+++ tests/commands_test/config_cmds_test.py
@@ -287,3 +287,95 @@ class TestSetCmd(CommandTestCase):
         self.assert_completion_candidates(SetCmd, ['set', 'foo', '_', '_', '_'], 2, ('a', 'b', 'c'))
         self.assert_completion_candidates(SetCmd, ['set', 'bar', '_', '_', '_'], 3, ('a', 'b', 'c'))
         self.assert_completion_candidates(SetCmd, ['set', 'baz', '_', '_', '_'], 4, ('a', 'b', 'c'))
+
+
+from stig.commands.cli import RateLimitCmd
+from asynctest import CoroutineMock
+class TestRateLimitCmd(CommandTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patch('stig.commands.cli.RateLimitCmd',
+                   srvapi=self.api)
+
+    async def test_call_syntaxes(self):
+        _set_limits = CoroutineMock()
+        _show_limits = CoroutineMock()
+        self.patch(RateLimitCmd, _set_limits=_set_limits, _show_limits=_show_limits)
+
+        await self.execute(RateLimitCmd, 'up', '1Mb', 'limit-rate-up<1k')
+        _set_limits.assert_awaited_once_with(['limit-rate-up<1k'], ('up',), '1Mb', adjust=False, quiet=False)
+        _show_limits.assert_not_awaited()
+        _set_limits.reset_mock() ; _show_limits.reset_mock()
+
+        await self.execute(RateLimitCmd, 'up', '1Mb')
+        _set_limits.assert_awaited_once_with([], ('up',), '1Mb', adjust=False, quiet=False)
+        _show_limits.assert_not_awaited()
+        _set_limits.reset_mock() ; _show_limits.reset_mock()
+
+        await self.execute(RateLimitCmd, 'up')
+        _set_limits.assert_not_awaited()
+        _show_limits.assert_awaited_once_with([], ('up',))
+        _set_limits.reset_mock() ; _show_limits.reset_mock()
+
+        await self.execute(RateLimitCmd, 'up', 'show', 'limit-rate-up<1k')
+        _set_limits.assert_not_awaited()
+        _show_limits.assert_awaited_once_with(['limit-rate-up<1k'], ('up',))
+        _set_limits.reset_mock() ; _show_limits.reset_mock()
+
+    async def test_setting_global_limits(self):
+        for direction in ('up', 'dn', 'down'):
+            real_dir = {'dn':'down'}.get(direction, direction)
+            set_method = 'set_limit_rate_' + real_dir
+            adjust_method = 'adjust_limit_rate_' + real_dir
+
+            self.api.settings.forget_calls()
+            process = await self.execute(RateLimitCmd, direction, '1Mb', 'global')
+            self.assertEqual(process.success, True)
+            self.api.settings.assert_called(1, set_method, ('1Mb',), {})
+            self.assert_stdout('ratelimit: Global %sload rate limit: None' % real_dir)
+            self.assert_stderr()
+            self.clear_stdout(); self.clear_stderr()
+
+            self.api.settings.forget_calls()
+            process = await self.execute(RateLimitCmd, direction, '+=2MB', 'global')
+            self.assertEqual(process.success, True)
+            self.api.settings.assert_called(1, adjust_method, ('+2MB',), {})
+            self.assert_stdout('ratelimit: Global %sload rate limit: None' % real_dir)
+            self.assert_stderr()
+            self.clear_stdout(); self.clear_stderr()
+
+            self.api.settings.forget_calls()
+            process = await self.execute(RateLimitCmd, direction, '--', '-=10Mb', 'global')
+            self.assertEqual(process.success, True)
+            self.api.settings.assert_called(1, adjust_method, ('-10Mb',), {})
+            self.assert_stdout('ratelimit: Global %sload rate limit: None' % real_dir)
+            self.assert_stderr()
+            self.clear_stdout(); self.clear_stderr()
+
+            self.api.settings.forget_calls()
+            self.api.settings.raises = ValueError('bad value')
+            process = await self.execute(RateLimitCmd, direction, 'fooo', 'global')
+            self.assertEqual(process.success, False)
+            self.assert_stdout()
+            self.assert_stderr("ratelimit: bad value: 'fooo'")
+            self.clear_stdout(); self.clear_stderr()
+
+    def test_completion_candidates_directions(self):
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', ''], 1, ('up', 'down'), (',',))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up,'], 1, ('down',), (',',))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'down,'], 1, ('up',), (',',))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'dn,'], 1, ('up',), (',',))
+
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', ''], 1, ('up', 'down'), (',',))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', '', '--quiet'], 1, ('up', 'down'), (',',))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', '--quiet', ''], 2, ('up', 'down'), (',',))
+
+    @patch('stig.commands.base.config.candidates')
+    def test_completion_candidates_torrent_filter(self, mock_candidates):
+        mock_candidates.torrent_filter.return_value = ('a', 'b', 'c')
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up', '10MB', '_'], 3, ('a', 'b', 'c'))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up', '10MB', '_', '_'], 4, ('a', 'b', 'c'))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up', '10MB', '_', '_'], 3, ('a', 'b', 'c'))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', '-q', 'up', '10MB', '_', '_'], 4, ('a', 'b', 'c'))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up', '-q', '10MB', '_', '_'], 5, ('a', 'b', 'c'))
+        self.assert_completion_candidates(RateLimitCmd, ['ratelimit', 'up', '10MB', '-q', '_', '_'], 4, ('a', 'b', 'c'))
