8dde8c39fadb539326666401c01551f8c7befd5f
Random User <rndusr@posteo.de>
2020-05-20 16:24:28 +0200

    Move cached_property tests out of client tests

--- tests/client_test/client_utils_test.py
+++ tests/client_test/client_utils_test.py
@@ -1,33 +1,7 @@
-from stig.client import utils
-
 import unittest
 from unittest.mock import MagicMock, call
 
-
-class Test_cached_property(unittest.TestCase):
-    def test_no_arguments(self):
-        foo = MagicMock(return_value='bar')
-        class X:
-            @utils.cached_property
-            def foo(self):
-                return foo()
-        x = X()
-        for _ in range(5):
-            self.assertEqual(x.foo, 'bar')
-        foo.assert_called_once_with()
-
-    def test_after_creation(self):
-        foo = MagicMock(return_value='bar')
-        callback = MagicMock()
-        class X:
-            @utils.cached_property(after_creation=callback)
-            def foo(self):
-                return foo()
-        x = X()
-        for _ in range(5):
-            self.assertEqual(x.foo, 'bar')
-        foo.assert_called_once_with()
-        callback.assert_called_once_with(x)
+from stig.client import utils
 
 
 class TestURL(unittest.TestCase):
