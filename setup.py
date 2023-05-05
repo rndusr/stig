a87d40a32d7717e4620a92b3adadcd714ccfb994
Random User <rndusr@posteo.de>
2023-05-05 11:04:52 +0200

    setup.py: Update supported Python versions

--- setup.py
+++ setup.py
@@ -68,6 +68,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Environment :: Console',
         'Operating System :: Unix',
         'Development Status :: 3 - Alpha',
